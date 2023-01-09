12063ae97712f4414c84f853552cfb0855fdba90
andres3m <andresmartinezuk@gmail.com>
2023-01-09 12:30:35 +0000

    Navigation component added on list, edit and view pages

--- imaginerium/pages/characterList.js
+++ imaginerium/pages/characterList.js
@@ -4,9 +4,12 @@ import Navigation from "../Components/Navigation";
 
 export default function characterList() {
     return (
+        <>
+            <Navigation></Navigation>
       <div>
         <h1>List of characters</h1>
           <button><Link href="/">← Back to home</Link></button>
       </div>
+      </>
     );
   }
--- imaginerium/pages/createCharacter.js
+++ imaginerium/pages/createCharacter.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import Navigation from "../Components/Navigation";
 
 
 export default function createCharacter() {
     return (
-      <div>
+        <>
+        <Navigation></Navigation>
+        <div>
         <h1>Create character</h1>
           <button><Link href="/">← Back to home</Link></button>
       </div>
+      </>
     );
   }
