7ea7f8a1719293643e2c50a9b125f524bb4b056a
andres3m <andresmartinezuk@gmail.com>
2023-01-10 14:00:04 +0000

    Checkbox functionality in progress

--- imaginerium/pages/createCharacter.js
+++ imaginerium/pages/createCharacter.js
@@ -2,16 +2,30 @@ import Link from "next/link";
 import Navigation from "../Components/Navigation";
 // import NameInput from "../Components/NameInput";
 import Input from "../Components/Input";
+import Button from "../Components/Button";
+import { useState } from "react";
 
 
 export default function createCharacter() {
+  const[isAlive, setIsAlive] = useState(false);
+  console.log(isAlive)
+  
+  function handlecheckbox(){
+    setIsAlive(isAlive == true ? false : true)
+    return isAlive
+  } 
+
     return (
         <>
         <Navigation></Navigation>
         <div>
         <h1>Create character</h1>
-        <Input></Input>
-          <button><Link href="/">← Back to home</Link></button>
+        <Input placeholder ="Enter character name" type="text"></Input>
+        <Input placeholder ="Enter character height cm" type="number"></Input> {/*To limit to positive numbers only */}
+        <Input placeholder ="Enter character age" type="number"></Input>
+        <p>Is your character alive?</p><Input type="checkbox" onClick={handlecheckbox}/>
+        {isAlive}
+        <Button text="Submit Character"/>
       </div>
       </>
     );
