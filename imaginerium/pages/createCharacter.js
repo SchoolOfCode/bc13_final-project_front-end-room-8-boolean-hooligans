e4a3132315edde4a5623af96e30ed12c36591811
Fliss317 <felicity.cullen@mail.com>
2023-01-11 12:08:39 +0000

    updating character creation, POST route works

--- imaginerium/pages/createCharacter.js
+++ imaginerium/pages/createCharacter.js
@@ -18,40 +18,52 @@ export default function createCharacter() {
     <>
        <Navigation></Navigation> 
       <div>
-        <h1>Create character</h1>
+        <h1>Create Your Character Here:</h1>
         <form
           onSubmit={(e) => {
             e.preventDefault();
             const formData = new FormData(e.target);
             const characterData = {
               user_id: 1,
-              char_name: formData.get("characterName"),
-              char_height: formData.get("characterHeight"),
-              char_age: formData.get("characterAge"),
-              char_alive: formData.get("characterAlive"),
+              char_name: formData.get("char_name"),
+              char_height: formData.get("char_height"),
+              char_age: formData.get("char_age"),
+              char_alive: formData.get("char_alive"),
             };
 
             addNewCharacter(characterData);
+
+            alert(`You have created ${characterData.char_name}`)
           }}
         >
-          <input
-            placeholder="Enter character name"
-            type="text"
-            name="characterName"
-          />
-          <input
-            placeholder="Enter character height feet"
-            type="number"
-            name="characterHeight"
-          />
+          <label htmlFor="char_name">Character Name:</label>
+          <input type="text" name="char_name" id="char_name"/>
+          
+          <label htmlFor="char_height">Height in feet?</label>
+          <select className='dropdown' name="char_height" id="char_height">
+          <option value="" defaultValue>How tall are they?</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          </select>
+
           {/*change to limit to positive numbers only */}
-          <input
-            placeholder="Enter character age"
-            type="number"
-            name="characterAge"
-          />
-          <p>Is your character alive?</p>
-          <input type="checkbox" name="characterAlive" />
+          <label htmlFor="char_age">How old are they in years?</label>
+          <input type="number" name="char_age" id="char_age"/>
+
+          <label htmlFor="char_alive">Are they alive?</label>
+          <select className='dropdown' name="char_alive" id="char_alive">
+          <option value="true">They live!</option>
+          <option value="false">They are dead!</option>
+          </select>
+
           <button id="next">Submit</button>
         </form>
       </div>
