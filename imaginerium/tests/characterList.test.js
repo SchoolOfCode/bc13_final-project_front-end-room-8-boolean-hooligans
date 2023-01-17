e4e552313373078fe90b613b04b989290a11f945
andres3m <andresmartinezuk@gmail.com>
2023-01-17 10:58:27 +0000

    Character list test attempt

--- imaginerium/tests/characterList.test.js
+++ imaginerium/tests/characterList.test.js
@@ -1,29 +1,21 @@
-import CharacterCard from "../pages/characterList.js";
+import characterList from "../pages/characterList.js";
 import { test, expect } from "@jest/globals";
 import { render, screen } from "@testing-library/react";
 
-// will CharacterCard render correctly 
-describe('CharacterCard', () => {
-    it('renders a card', () => {
-      render(<CharacterCard />)
+
+
+  describe('Character List', () => {
+    it('renders the Character List', () => {
+      render(<characterList />)
   
-      expect(CharacterCard).toBeInTheDocument()
+      const characterCard = screen.getByRole('characterCard', {
+        name: /cctest\.js!/i,}
+        )
+  
+      expect(characterCard).toBeInTheDocument()
     })
   })
 
 
 
 
-//test for fetchData 
-/*test("making sure fetchData brings back an array",  async () => {
-    const response = await supertest(_app).fetch(`https://imaginerium-qpii.onrender.com/characters`);
-    expect(response.json).toBe(JSON);
-    expect(data.payload).toBe(Array)
-});
-
-// test to make sure searchByName works 
-test("searchByName functions correctly", async () => {
-    const response = await supertest(_app).get("/characters?char_name=");
-    expect(response.json).toBe(JSON);
-    expect(data.payload).toBe(Array);
-});*/
