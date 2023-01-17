ec6a789df57de3e63e983caac5578d75d945a0a2
andres3m <andresmartinezuk@gmail.com>
2023-01-17 13:46:05 +0000

    index and character list working

--- imaginerium/tests/characterList.test.js
+++ imaginerium/tests/characterList.test.js
@@ -1,18 +1,18 @@
 import characterList from "../pages/characterList.js";
 import { test, expect } from "@jest/globals";
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom' 
+import CharacterCard from "../Components/CharacterCard.js";
 
 
 
-  describe('Character List', () => {
-    it('renders the Character List', () => {
-      render(<characterList />)
+  describe('Character Card', () => {
+    it('renders the Character card', () => {
+      render(<CharacterCard character_alive={false}/>)
   
-      const characterCard = screen.getByRole('characterCard', {
-        name: /cctest\.js!/i,}
-        )
+      const cardsContainer = screen.getByText('Dead')
   
-      expect(characterCard).toBeInTheDocument()
+      expect(cardsContainer).toBeInTheDocument()
     })
   })
 
