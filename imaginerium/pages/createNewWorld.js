77d879b3b7505851857fec4ca9340372281ea9a4
Fliss317 <felicity.cullen@mail.com>
2023-01-18 10:48:19 +0000

    prep for starting again

--- imaginerium/pages/createNewWorld.js
+++ imaginerium/pages/createNewWorld.js
@@ -1,10 +1,48 @@
 import WorldmapGenerator from 'worldmap-generator/controllers/WorldMap';
 import Image from 'next/image';
 import Navigation from '../Components/Navigation.js';
-import Button from '../Components/Button.js'
+import Button from '../Components/Button.js';
+// import co from 'co';
+// import helper from '../Components/maphelper.js';
+import fs from 'fs-extra';
+import chalk from 'chalk';
+import { Generator } from 'random-map-generator';
 
-export default function createNewWorld() {
+export default async function createNewWorld() {
+    const seed = null;
+
+    const mapGenerator = new Generator(100, 100, seed);
+    mapGenerator.setLandmassStepperCount(100);
+    mapGenerator.setLandmassStepperStep(150);
+    mapGenerator.setRivers(1, 2);
+    await mapGenerator.generate();
+
+    mapGenerator.outputToConsole();
+
+    // eslint-disable-next-line
+    console.log(chalk.green(`Seed: ${mapGenerator.seed}`));
+
+    fs.writeFileSync(
+        'example-map.json',
+        JSON.stringify(mapGenerator.matrix),
+    );
+
+return (
+    <>
+    <Navigation />
+    <Button onClick={createNewWorld()}>{text="Generate World Map"}</Button>
+    <Image src={{filename}}/> 
+    </>
+)
+
+}
+
+
+/*export default function* createNewWorld() {
+
+const filename = __dirname + '/result.png';
 // create world with some params
+console.time("Generator initiated")
 const world = new WorldmapGenerator({
     size: {
         width:300, // map width
@@ -36,15 +74,25 @@ const world = new WorldmapGenerator({
     }
     ]},
 );
+console.timeEnd("Generation initiation complete")
 
-// world.generate()
+// generate world
+console.time("World generated");
+world.generate();
+console.timeEnd("World generated");
+
+// create image
+console.time(`World saved to ${filename}`);
+const image = yield helper.createImageFromArray(world.map, WIDTH, HEIGHT);
+yield helper.writeImage(filename, image);
+console.timeEnd(`World saved to ${filename}`);
 
 return (
     <>
     <Navigation />
-    <button onClick={console.log("the button works")}>Generate World Map</button>
-    {/* {world.map} */}
+    <Button onClick={createNewWorld()}>{text="Generate World Map"}</Button>
+    <Image src={{filename}}/>
     </>
 
 )
-}
+}*/
