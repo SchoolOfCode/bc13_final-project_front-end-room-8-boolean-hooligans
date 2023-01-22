62c3fcd1597db81c8a79d2c3f7e19afc8366db9f
Fliss317 <felicity.cullen@mail.com>
2023-01-22 17:36:10 +0000

    minor additions

--- imaginerium/pages/aboutUs.js
+++ imaginerium/pages/aboutUs.js
@@ -8,15 +8,15 @@ return (
     <div>
     <h1>The Boolean Hooligans</h1>
     <p>We are a team of five from the School of Code Cohort 13, and this site is our final project. We built it over the course of four weeks in January 2023, and are very pleased with the end result!</p>
-    <h2>Felicity Cullen:</h2>
+    <h2>Felicity Cullen</h2>
     <p></p>
-    <h2>Andres Espin Martinez:</h2>
+    <h2>Andres Espin Martinez</h2>
     <p></p>
-    <h2>Iona Rose:</h2>
+    <h2>Iona Rose</h2>
     <p></p>
-    <h2>Filip Ruminski:</h2>
+    <h2>Filip Ruminski</h2>
     <p></p>
-    <h2>Charles Sajan:</h2>
+    <h2>Charles Sajan</h2>
     <p></p>
     </div>
     </>
