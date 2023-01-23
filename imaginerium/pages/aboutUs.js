3dc9457617b61879cd612cab5577d11befe10d76
Fliss317 <felicity.cullen@mail.com>
2023-01-23 10:12:03 +0000

    photos of us added, cards finalised

--- imaginerium/pages/aboutUs.js
+++ imaginerium/pages/aboutUs.js
@@ -27,20 +27,12 @@ return (
           <div className={styles.card}>
             <div className={styles.additional}>
               <div className={styles.userCard}>
+                <h2>Felicity Cullen</h2>
                 <div className={styles.aliveStatus}>
                     <span>
                       <GiHeartInside /> <span>Alive</span>
                     </span>
                 </div>
-    
-                <img
-                  className="image"
-                  src="https://deepgrouplondon.com/wp-content/uploads/2019/06/person-placeholder-5.png"
-                />
-              </div>
-              <div className={styles.moreInfo}>
-                <h1>Felicity Cullen</h1>
-    
                 <div className="stats">
                   <div>
                   <p><Link href="https://www.linkedin.com/in/felicitycullen"> LinkedIn</Link></p>
@@ -50,45 +42,161 @@ return (
                   </div>
                 </div>
               </div>
+              <div className={styles.moreInfo}>
+                <p className="description">Prior to the bootcamp, Felicity spent 15 years in a variety of retail, office, and other service jobs before deciding to take the plunge into tech and do something constructive with the potential to improve the lives of many people. She enjoys back-end development and in her free time likes to indulge her nerdy hobbies such as painting toy soldiers and playing boardgames, and occasionally writing poetry. </p>
+              </div>
             </div>
             <div className={styles.general}>
-              <h1>Felicity Cullen</h1>
-              <p className="description">Prior to the bootcamp, Felicity spent 15 years in a variety of retail, office, and other service jobs before deciding to take the plunge into tech and do something constructive with the potential to improve the lives of many people. She enjoys back-end development and in her free time likes to indulge her nerdy hobbies such as painting toy soldiers and playing boardgames, and occasionally writing poetry. </p>
-              {/* <span className="more">Click to expand</span> */}
+                <img
+                  className="image"
+                  src="./felicity_pfp.jpg"
+                  alt="A photo of Felicity, holding a toy soldier. She is smiling."
+                />
             </div>
           </div>
     </div>  
     </div> 
 
-    <div className="card green">
-        <h2>Andres Espin Martinez</h2>
-        <p>WORDS GO HERE</p>
-        <p><Link href="https://www.linkedin.com/in/andres-e-036492108/">LinkedIn</Link></p>
-        <p><Link href="">GitHub</Link></p>
-    </div>
+    <div>
+        <div className="center">
+          <div className={styles.card}>
+            <div className={styles.additional}>
+              <div className={styles.userCard}>
+                <h2>Andrés Espin</h2>
+                <div className={styles.aliveStatus}>
+                    <span>
+                      <GiHeartInside /> <span>Alive</span>
+                    </span>
+                </div>
+                <div className="stats">
+                  <div>
+                  <p><Link href="https://www.linkedin.com/in/andres-e-036492108/"> LinkedIn</Link></p>
+                  </div>
+                  <div>
+                  <p><Link href="https://github.com/andres3m">GitHub</Link></p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.moreInfo}>
+                <p className="description">WORDS HERE </p>
+              </div>
+            </div>
+            <div className={styles.general}>
+                <img
+                  className="image"
+                  src="./andres_pfp.png"
+                  alt="Pixel art of Andrés."
+                />
+            </div>
+          </div>
+    </div>  
+    </div> 
 
-    <div className="card green">
-        <h2>Iona Rose</h2>
-        <p>WORDS GO HERE</p>
-        <p><Link href="linkedin.com/in/iona-rose/">LinkedIn</Link></p>
-        <p><Link href="https://github.com/ionarose">GitHub</Link></p> 
-    </div>
+    <div>
+        <div className="center">
+          <div className={styles.card}>
+            <div className={styles.additional}>
+              <div className={styles.userCard}>
+                <h2>Iona Rose</h2>
+                <div className={styles.aliveStatus}>
+                    <span>
+                      <GiHeartInside /> <span>Alive</span>
+                    </span>
+                </div>
+                <div className="stats">
+                  <div>
+                  <p><Link href="linkedin.com/in/iona-rose/"> LinkedIn</Link></p>
+                  </div>
+                  <div>
+                  <p><Link href="https://github.com/ionarose">GitHub</Link></p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.moreInfo}>
+                <p className="description">WORDS HERE </p>
+              </div>
+            </div>
+            <div className={styles.general}>
+                <img
+                  className="image"
+                  src="./iona_pfp.jpg"
+                  alt="A photo of Iona."
+                />
+            </div>
+          </div>
+    </div>  
+    </div> 
 
-    <div className="card green">
-        <h2>Filip Ruminski</h2>
-        <p>WORDS GO HERE</p>
-        <p><Link href="https://www.linkedin.com/in/filip-ruminski/">LinkedIn</Link></p>
-        <p><Link href="https://github.com/fruminski">GitHub</Link></p>
-    </div>
+    <div>
+        <div className="center">
+          <div className={styles.card}>
+            <div className={styles.additional}>
+              <div className={styles.userCard}>
+                <h2>Filip Ruminski</h2>
+                <div className={styles.aliveStatus}>
+                    <span>
+                      <GiHeartInside /> <span>Alive</span>
+                    </span>
+                </div>
+                <div className="stats">
+                  <div>
+                  <p><Link href="https://www.linkedin.com/in/filip-ruminski/"> LinkedIn</Link></p>
+                  </div>
+                  <div>
+                  <p><Link href="https://github.com/fruminski">GitHub</Link></p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.moreInfo}>
+                <p className="description">WORDS HERE </p>
+              </div>
+            </div>
+            <div className={styles.general}>
+                <img
+                  className="image"
+                  src="./filip_pfp.png"
+                  alt="Pixel art of Filip."
+                />
+            </div>
+          </div>
+    </div>  
+    </div> 
 
-    <div className="card green">
-        <h2>Charles Sajan</h2>
-        <p>WORDS GO HERE</p>
-        <p><Link href="https://www.linkedin.com/in/charlessajan/">LinkedIn</Link></p>
-        <p><Link href="">GitHub</Link></p>
+    <div>
+        <div className="center">
+          <div className={styles.card}>
+            <div className={styles.additional}>
+              <div className={styles.userCard}>
+                <h2>Charles Sajan</h2>
+                <div className={styles.aliveStatus}>
+                    <span>
+                      <GiHeartInside /> <span>Alive</span>
+                    </span>
+                </div>
+                <div className="stats">
+                  <div>
+                  <p><Link href="https://www.linkedin.com/in/charlessajan/"> LinkedIn</Link></p>
+                  </div>
+                  <div>
+                  <p><Link href="">GitHub</Link></p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.moreInfo}>
+                <p className="description">WORDS HERE </p>
+              </div>
+            </div>
+            <div className={styles.general}>
+                <img
+                  className="image"
+                  src="./charles_pfp.jpg"
+                  alt="A photo of Charles."
+                />
+            </div>
+          </div>
+    </div>  
     </div>
     </div>
-
     </div>
     </>
 )
