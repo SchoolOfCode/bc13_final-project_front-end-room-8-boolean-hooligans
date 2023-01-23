b286fb2e8eee70103cae4024741810e027103353
Fliss317 <felicity.cullen@mail.com>
2023-01-23 12:50:18 +0000

    About Us styled

--- imaginerium/pages/aboutUs.js
+++ imaginerium/pages/aboutUs.js
@@ -19,7 +19,7 @@ return (
     <div className={styles.headerBlock}>
     <p className={styles.headerText}>We are the BOOLEAN HOOLIGANS: a team of five from the <Link href="https://www.schoolofcode.co.uk/">School of Code's</Link> Cohort 13, and this site is our final project. We built it over the course of four weeks in January 2023, and are very pleased with the end result!<br></br><br></br>
     Our brief was to come up with a problem, and solve it. After some thinking, we decided to look at creating a simple to use worldbuilding app for creative people such as writers, artists, or roleplaying gamers. The existing options are often confusing, busy, or otherwise suffer from unfriendly UI/UX.<br></br><br></br>
-    The result is IMAGINERIUM, which in its current form allows you to create and save all sorts of original characters, collaborate with other creators, and - thanks to Azgaar's Fantasy Map Generator - make and save world maps. There are many other things we'd like to add in future, from random plot generation to some form of character illustration, along the lines of a picrew. But for now, enjoy the result of our hard work as full stack developers!</p>
+    The result is IMAGINERIUM, which in its current form allows you to create and save all sorts of original characters, collaborate with other creators, and - thanks to <Link href="https://github.com/Azgaar/Fantasy-Map-Generator">Azgaar's Fantasy Map Generator</Link> - make and save world maps. There are many other things we'd like to add in future, from random plot generation to some form of character illustration, along the lines of a picrew. But for now, enjoy the result of our hard work as full stack developers!</p>
     </div>
 
     <div className={styles.cardsContainer}>
@@ -45,12 +45,12 @@ return (
                 </div>
               </div>
               <div className={styles.moreInfo}>
-                <p className="description">Prior to the bootcamp, Felicity spent 15 years in a variety of retail, office, and other service jobs before deciding to take the plunge into tech and do something constructive with the potential to improve the lives of many people. She enjoys back-end development and in her free time likes to indulge her nerdy hobbies such as painting toy soldiers and playing boardgames, and occasionally writing poetry. </p>
+                <p className={styles.description}>Prior to the bootcamp, Felicity spent 15 years in a variety of retail, office, and other service jobs before deciding to take the plunge into tech and do something constructive with the potential to improve the lives of many people.</p>
               </div>
             </div>
             <div className={styles.general}>
                 <img
-                  className="image"
+                  className={styles.image}
                   src="./felicity_pfp.jpg"
                   alt="A photo of Felicity, holding a toy soldier. She is smiling."
                 />
@@ -80,12 +80,12 @@ return (
                 </div>
               </div>
               <div className={styles.moreInfo}>
-                <p className="description">WORDS HERE </p>
+                <p className={styles.description}>Andrés has seven years of experience working with hardware as an electronics technician. However, when he discovered the power of the web he joined the School of Code to learn new skills and transfer his current ones into the land of software. </p>
               </div>
             </div>
             <div className={styles.general}>
                 <img
-                  className="image"
+                  className={styles.image2}
                   src="./andres_pfp.png"
                   alt="Pixel art of Andrés."
                 />
@@ -115,12 +115,12 @@ return (
                 </div>
               </div>
               <div className={styles.moreInfo}>
-                <p className="description">WORDS HERE </p>
+                <p className={styles.description}>WORDS HERE </p>
               </div>
             </div>
             <div className={styles.general}>
                 <img
-                  className="image"
+                  className={styles.image2}
                   src="./iona_pfp.jpg"
                   alt="A photo of Iona."
                 />
@@ -150,12 +150,12 @@ return (
                 </div>
               </div>
               <div className={styles.moreInfo}>
-                <p className="description">WORDS HERE </p>
+                <p className={styles.description}>WORDS HERE </p>
               </div>
             </div>
             <div className={styles.general}>
                 <img
-                  className="image"
+                  className={styles.image2}
                   src="./filip_pfp.png"
                   alt="Pixel art of Filip."
                 />
@@ -185,12 +185,12 @@ return (
                 </div>
               </div>
               <div className={styles.moreInfo}>
-                <p className="description">WORDS HERE </p>
+                <p className={styles.description}>WORDS HERE </p>
               </div>
             </div>
             <div className={styles.general}>
                 <img
-                  className="image"
+                  className={styles.image3}
                   src="./charles_pfp.jpg"
                   alt="A photo of Charles."
                 />
