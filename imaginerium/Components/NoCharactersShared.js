4f8fa72494acfbc42e9441576d2c5ec05dce0bf5
Fliss317 <felicity.cullen@mail.com>
2023-01-24 15:05:10 +0000

    components added

--- imaginerium/Components/NoCharactersShared.js
+++ imaginerium/Components/NoCharactersShared.js
@@ -10,7 +10,7 @@ return (
     <>
         <NewNavBar/>
             <div>
-            <h1 className={styles.h1}>You have not yet made any characters! <Link href="/createCharacter">Why not create one now</Link>?</h1>
+            <h1 className={styles.h1}>You have not yet shared any characters! <Link href="/characterList">Why not share some now</Link>?</h1>
             <LoginOutButton />
             </div>
     </>
