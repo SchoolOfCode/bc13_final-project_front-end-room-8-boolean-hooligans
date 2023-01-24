0f0f02a76bb71ac4ec46457544cf186371fd4c55
andres3m <andresmartinezuk@gmail.com>
2023-01-24 11:02:43 +0000

    Buttons aligned, and temporary backgroundcolors added
    Co-authored-by: Charles <csajan360@gmail.com>

--- imaginerium/pages/dashboard.js
+++ imaginerium/pages/dashboard.js
@@ -11,7 +11,7 @@ import { VscPersonAdd, VscOrganization, VscGlobe } from "react-icons/vsc";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
- return (
+  return (
     <>
       <Head>
         <title>Imaginerium</title>
@@ -20,68 +20,60 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.homeContainer}>
-      {/* <Navigation /> */}
-      <NewNavBar />
-      <main className={styles.main}>
-      <div className={styles.titleContainer}>
-        <h1>IMAGINERIUM</h1>
-        <div className={styles.descriptionContainer}>
-          <Image
-            priority
-              src="https://i.ibb.co/ZVj6bcP/spinning-Earth-unscreen.gif"
-            className={styles.earth}
-            height={320}
-            width={ 427}
-            alt="Earth"
-          />
-         
-          <p>
-            Breathe life into your imagination with <span className={styles.background}>IMAGINERIUM</span> the ultimate creative playground for world building, character creation, and story weaving.
-          </p>
-          
-        </div>
+        {/* <Navigation /> */}
+        <NewNavBar />
+        <main className={styles.main}>
+          <div className={styles.titleContainer}>
+            <h1>IMAGINERIUM</h1>
+            <div className={styles.descriptionContainer}>
+              <Image
+                priority
+                src="https://i.ibb.co/ZVj6bcP/spinning-Earth-unscreen.gif"
+                className={styles.earth}
+                height={320}
+                width={427}
+                alt="Earth"
+              />
 
-        <div className={styles.buttonsContainer}>
-       
-          <Link href="/createCharacter">
-          <button className={styles.buttonMain}>
-            <div>
-            create character
+              <p>
+                Breathe life into your imagination with{" "}
+                <span className={styles.background}>IMAGINERIUM</span> the
+                ultimate creative playground for world building, character
+                creation, and story weaving.
+              </p>
             </div>
-            <VscPersonAdd className={styles.add}/>
-          </button>
-          </Link>     
 
-        <Link href="/characterList" >
-         <button className={styles.buttonMain}>
-          <div className={styles.buttonMain}>
-          your characters
+            <div className={styles.buttonsContainer}>
+              <Link href="/createCharacter">
+                <button className={styles.buttonMain}>
+                  <div>create character</div>
+                  <VscPersonAdd className={styles.add} />
+                </button>
+              </Link>
+
+              <Link href="/characterList">
+                <button className={styles.buttonMain}>
+                  <div className={styles.buttonMain}>your characters</div>
+                  <VscOrganization className={styles.add} />
+                </button>
+              </Link>
+
+              <Link href="/sharedCharacters">
+                <button className={styles.buttonMain}>
+                  <div className={styles.buttonMain}>shared characters</div>
+                  <VscOrganization className={styles.add} />
+                </button>
+              </Link>
+
+              <Link href="/createNewWorld">
+                <button className={styles.buttonMain}>
+                  <div>create world</div>
+                  <VscGlobe className={styles.add} />
+                </button>
+              </Link>
+            </div>
           </div>
-          <VscOrganization className={styles.add}/>
-          </button>
-          </Link>
-
-          <Link href="/sharedCharacters" >
-         <button className={styles.buttonMain}>
-          <div className={styles.buttonMain}>
-          shared characters
-          </div>
-          <VscOrganization className={styles.add}/>
-          </button>
-          </Link>
-
-          <Link href="/createNewWorld">
-          <button className={styles.buttonMain}>
-          <div>
-          create world
-          </div>
-          <VscGlobe className={styles.add}/>
-          </button>
-          </Link>
-
-        </div>
-        </div>
-      </main>
+        </main>
       </div>
     </>
   );
