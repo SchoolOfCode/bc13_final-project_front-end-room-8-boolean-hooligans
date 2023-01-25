8643f952a7cb475cf59c233b719fb5933a0e5aca
andres3m <andresmartinezuk@gmail.com>
2023-01-25 21:35:10 +0000

    Unused login options removed

--- imaginerium/pages/api/auth/[...nextauth].js
+++ imaginerium/pages/api/auth/[...nextauth].js
@@ -19,10 +19,6 @@ export const authOptions = {
     }),
     */
 
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
