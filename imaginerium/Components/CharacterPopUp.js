c6b6f000519f143ee3410899717d2b3ea07621e7
ionarose <ionaroseuk@gmail.com>
2023-01-25 10:21:18 +0000

    share function fixed

--- imaginerium/Components/CharacterPopUp.js
+++ imaginerium/Components/CharacterPopUp.js
@@ -156,7 +156,7 @@ export default function CharacterPopUp(props) {
                       )
                     ) {
                       {
-                        shareCharacter;
+                        shareCharacter();
                       }
                     }
                   }}
