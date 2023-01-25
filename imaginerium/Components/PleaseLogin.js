2ebe33ad4ef22f9c34fcc275504db379e9a2b0ad
ionarose <ionaroseuk@gmail.com>
2023-01-25 20:48:23 +0000

    squares!

--- imaginerium/Components/PleaseLogin.js
+++ imaginerium/Components/PleaseLogin.js
@@ -1,19 +1,44 @@
 import NewNavBar from "./NewNavBar.js";
-import LoginOutButton from "./login-btn";
+import { AiOutlineLogin } from "react-icons/ai";
 import { useSession } from "next-auth/react";
 import styles from "./../styles/pleaseLogin.module.css";
+import Link from "next/link";
 
 export default function PleaseLogin() {
-    const { data: session } = useSession();
-    if (!session) {
+  const { data: session } = useSession();
+  if (!session) {
     return (
-        <>
-            <NewNavBar/>
-            <div>
-            <h1 className={styles.h1}>You are not signed in! Please sign in:</h1>
-            <LoginOutButton />
-            </div>
-        </>
-        );
-    }
+      <>
+        <NewNavBar />
+        <Link className={styles.buttonText} href="/">
+                <button className={styles.buttonMain}>
+                  <div><AiOutlineLogin className={styles.icon} /></div>
+                  <div>Log in</div>
+                </button>
+              </Link>
+        <div className={styles.body}>
+          <div className={styles.context}>
+            
+             <h1>IMAGINERIUM</h1>
+            <h2>Log in to see saved content</h2>
+          </div>
+
+          <div className={styles.area}>
+            <ul className={styles.circles}>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
