4bf0fa121d0cbc10cea00d21b62bf6b67189f8f3
Charles <csajan360@gmail.com>
2023-01-26 10:38:22 +0000

    Commit before working on login page

--- imaginerium/cypress/e2e/spec.cy.js
+++ imaginerium/cypress/e2e/spec.cy.js
@@ -1,80 +1,32 @@
-describe('Visits pages', () => {
-  it('Main page', () => {
+describe('Visits start page', () => {
+  it('Home page loaded', () => {
     cy.visit('http://localhost:3000/')
+    cy.get(':nth-child(1) > h1')
+    cy.get('.landing_earth__WoHeK')
   })
-  it('Create character page', () => {
+  it('Starting page contains title', () => {
     cy.visit('http://localhost:3000/')
-    cy.get("button").contains("Create Character").should("have.attr", "href", "/createCharacter").click()
+    cy.get(':nth-child(1) > h1')
   })
-  it('View character page', () => {
+  it('Starting page contains earth', () => {
     cy.visit('http://localhost:3000/')
-    cy.get('.Home_buttonsContainer___okSV > :nth-child(2)').click()
+    cy.get('.landing_earth__WoHeK')
   })
-
 })
 
-// import Navigation from "../../Components/Navigation.js"
-// describe('Navigation', () => {
-//   // cy.mount(<Navigation/>)
-//   beforeEach(() => {
-//   cy.visit('http://localhost:3000/');
-//   });
+describe('Logging in', () => {
+  it('Directed to login options', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.landing_earth__WoHeK').click()
+    cy.get('.card')
+  })
+  it('Login with google', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.landing_earth__WoHeK').click()
+    cy.get(':nth-child(3) > form > .button').click()
+    
+  })
+
   
-//   it('displays the logo', () => {
-//   cy.get('.navigation_nav__jLC69')
-//   .should('have.attr', 'src', 'https://i.ibb.co/0BtC8vF/imaginerium-logo2-1.png')
-//   .should('have.attr', 'height', '65')
-//   .should('have.attr', 'width', '212')
-//   .should('have.attr', 'alt', 'Imaginerium');
-//   });
-  
-//   it('displays the home button', () => {
-//   cy.get('.navButtons')
-//   .find('.dropdown')
-//   .first()
-//   .find('button')
-//   .should('have.text', 'HOME')
-//   .click();
-//   cy.url().should('include', '/');
-//   });
-  
-//   it('displays the create button and dropdown', () => {
-//   cy.get('.navButtons')
-//   .find('.dropdown')
-//   .eq(1)
-//   .find('button')
-//   .should('have.text', 'CREATE')
-//   .click();
-//   cy.get('.navButtons')
-//   .find('.dropdownContent')
-//   .should('have.text', 'Create character')
-//   .find('a')
-//   .should('have.attr', 'href', '/createCharacter');
-//   });
-  
-//   it('displays the view button and dropdown', () => {
-//   cy.get('.navButtons')
-//   .find('.dropdown')
-//   .eq(2)
-//   .find('button')
-//   .should('have.text', 'VIEW')
-//   .click();
-//   cy.get('.navButtons')
-//   .find('.dropdownContent')
-//   .should('have.text', 'Character list')
-//   .find('a')
-//   .should('have.attr', 'href', '/characterList');
-//   });
-  
-//   it('displays the login button', () => {
-//   cy.get('.loginButton')
-//   .should('have.text', 'LOGIN');
-//   });
-  
-//   it('displays the dark mode toggle button', () => {
-//   cy.get('.darkModeSwitch')
-//   .find(ButtonToggleDarkMode);
-//   });
-//   });
-  
+})
   
